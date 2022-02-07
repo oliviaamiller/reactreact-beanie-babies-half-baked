470f9cc65a1c969f5115f705997c4276a9eef94a
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-07 14:40:18 -0800

    added findbytext to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders current page link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.findByText(/current page/i);
   expect(linkElement).toBeInTheDocument();
 });
