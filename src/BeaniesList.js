6b2adaf28e4f1bd60a57437c92a48184abf5261a
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-07 12:16:13 -0800

    rendered beanie babies as list, passed props

--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -4,5 +4,7 @@ export default function BeaniesList({ beanieBabies }) {
   return (
     <div className='beanies'>
       {/* render out the beanie babies as a list */}
+      {beanieBabies.map((baby, i) => 
+        <BeanieBaby key={`${baby}-${i}`} baby={baby} />)}
     </div>);
 }
