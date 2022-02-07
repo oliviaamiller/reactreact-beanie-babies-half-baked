958679a14c45c87d79db87e0fa78ed12cdfc3f10
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-07 15:46:14 -0800

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders current page link', () => {
+test('renders current link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/current page/i);
+  const linkElement = screen.getByText(/current/i);
   expect(linkElement).toBeInTheDocument();
 });
